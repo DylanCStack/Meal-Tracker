304f95abc17fdff89f014ba2d7258eed601cc4ee
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-03-31 12:02:08 -0700

    add color change to new-meal box depending on how many calories are being inputted

--- src/app/new-meal/new-meal.component.ts
+++ src/app/new-meal/new-meal.component.ts
@@ -8,12 +8,25 @@ import { Meal } from '../meal.model';
 export class NewMealComponent implements OnInit {
   @Input() addingMeal:boolean;
   @Output() newMealSender = new EventEmitter();
+  divClass:string= "alert alert-success";
 
   constructor() { }
 
   ngOnInit() {
   }
-  
+  updateClass(cals){
+    var htmlClass:string = "alert alert-";
+    var mod:string = "success";
+    if(cals > 200 && cals<500){
+      mod = "info";
+    } else if(cals > 500 && cals<1000) {
+      mod = "warning";
+    } else if(cals > 1000){
+      mod="danger";
+    }
+    this.divClass = htmlClass + mod;
+  }
+
   trackMeal(name, details, calories){
     this.newMealSender.emit(new Meal(name, details, calories, Date.now()));
   }
