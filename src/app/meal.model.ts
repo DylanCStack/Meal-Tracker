923eca91cf6cf1de8185412adb3028ada7bc7689
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-03-31 10:50:17 -0700

    add display for time the meal was added to be tracked

--- src/app/meal.model.ts
+++ src/app/meal.model.ts
@@ -2,4 +2,6 @@ export class Meal{
 constructor(public name: string, public details: string, public calories: number, public date:number){}
 
 //add values to extrapolate date information into readable info (month, day)
+public d = new Date(this.date)
+public displayTime=this.d.getMonth()+1 + "/" + this.d.getDay() + " at " + this.d.getHours() + ":" + this.d.getMinutes();
 }
