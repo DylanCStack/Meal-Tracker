995e3bdce35df64152c654fd3caed50fe8a76f15
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-03-31 10:39:42 -0700

    add bootstrap styling classes

--- src/app/mock-meals.ts
+++ src/app/mock-meals.ts
@@ -1,7 +1,7 @@
 import { Meal } from "./meal.model";
 
 export const MEALS: Meal[]=[
-  new Meal("Burger", "Two extra patties with a milkshake.", 950, 1490973929155),
+  new Meal("Burger", "Two extra patties with a milkshake.", 1150, 1490973929155),
   new Meal("Vegetarian 'pizza'", "Just bread and ketchup.", 70, 1490975038633),
   new Meal("Full English Breakfast", "I swear I don't know how those extra sausages got on my plate... or off of it.", 700, 1490977038633)
 ]
